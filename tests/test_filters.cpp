1fcb23d955656fa599f62c8e6dcea149e3cf5991
Christopher Goebel <cmgoebel@iupui.edu>
2020-04-06 02:25:20 -0400

    fix namespace issue in entry
    Signed-off-by: Christopher Goebel <cmgoebel@iupui.edu>

--- tests/test_filters.cpp
+++ tests/test_filters.cpp
@@ -9,32 +9,10 @@
 
 #include <numeric>
 
-#define VERBOSE
 
 namespace
 {
   using namespace alf::types;
-
-  template<typename T, typename Container=std::deque<T>>
-  class iterable_queue : public std::queue<T, Container>
-  {
-  public:
-    typedef typename Container::iterator iterator;
-    typedef typename Container::const_iterator const_iterator;
-
-    std::queue<T> q_;
-
-    explicit iterable_queue(std::queue<T> q)
-      : std::queue<T>(q) { }
-
-    iterator begin() { return this->c.begin(); }
-
-    iterator end() { return this->c.end(); }
-
-    [[nodiscard]] const_iterator begin() const { return this->c.begin(); }
-
-    [[nodiscard]] const_iterator end() const { return this->c.end(); }
-  };
 
   auto print_vec(std::vector<alf::types::TokenBase> const& v)
   {
@@ -46,6 +24,7 @@ namespace
     }
     std::cout << "]\n";
   }
+
 
   TEST(TestFilters, all_and_found)
   {
